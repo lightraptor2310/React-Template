bcf94cec0c4262ef033385196d7d94066db10fc5
Duy Lê <duy.le@vnetwork.vn>
2023-11-09 10:04:05 +0700

    create page manage template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import {Button} from '@mui/material'
+
 
 function App() {
-
+  const list_button = [
+    {name: 'login', value : '/login'}
+  ]
   return (
     <>
-      <p className=" text-center">hello test router</p>
+      <p className=" text-center text-3xl font-semibold">Template prototype</p>
+      <p className='text-center p-5'>Page tổng hợp các template front end của nhóm kinh khủng</p>
+      <p className='text-xl m-3 font-semibold'>Danh sách page</p>
+      <div className="list-template grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-3">
+        {list_button.map((item)=>
+          
+          <Button variant="outlined" color="primary" href={`${item.value}`}>
+            {item.name}
+          </Button>
+      )}
+      </div>
+
     </>
   )
 }
