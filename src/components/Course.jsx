8b2281827a01320c73a549a3885fef083887207b
Duy Lê <duy.le@vnetwork.vn>
2023-11-17 13:26:46 +0700

    update background

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -5,10 +5,10 @@ const Course = () => {
   return (
     <>
       <div className="flex flex-col md:w-[75%] w-full items-center h-screen">
-        <div className="md:w-[99%] w-[99%] h-[86%] rounded-lg bg-sky-700 m-3 text-white p-10 flex flex-col justify-start items-start bg-[url('/public/vlsi.jpg')]">
-          <div className="flex flex-col hero-panel rounded-lg md:w-fit w-full">
-            <h1 className="md:text-5xl text-2xl font-base mt-3 ml-2 ">Hello, Kinh khung</h1>
-            <h1 className="md:text-5xl text-2xl font-base m-3 ml-2">Welcome to E-learning</h1>
+        <div className="md:w-[99%] w-[99%] h-[86%] rounded-lg m-3 text-white p-10 flex flex-col justify-center items-center bg-[url('chip.jpg')] bg-cover">
+          <div className=" md:p-5 flex flex-col hero-panel rounded-lg md:w-fit w-full">
+            <h1 className="md:text-3xl text-2xl font-base mt-3 ml-2 ">Hello, Kinh khung</h1>
+            <h1 className="md:text-3xl text-2xl font-base m-3 ml-2">Welcome to E-learning</h1>
           </div>
         </div>
       </div>
