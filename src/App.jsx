70ece93d35418e5fe795510ac51ac01833d13481
Duy Lê <duy.le@vnetwork.vn>
2023-12-29 15:53:55 +0700

    add home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import {Route, Routes} from 'react-router-dom'
+import {Link, Route, Routes} from 'react-router-dom'
 import Login from './page/Login'
 import Home from './page/Home'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
+import Button from '@mui/material/Button'
 const App = () => {
   const [value, setValue] = useState(new Date());
   return (
-    <><h1>Hello</h1>
-    <Calendar value={value}/></>
+    <><p>Trang chủ của nhóm kinh khủng</p>
+    <p>Bấm vào nút đăng nhập phía dưới</p>
+    <Button variant="contained" color="primary">
+    <Link to="/login">Đăng nhập</Link>
+    </Button>
+  
+    </>
   )
 }
 export default App
